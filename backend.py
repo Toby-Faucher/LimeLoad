6349d7109dc01fe2463f5239d39066ad12fdde4a
Toby <toby@tfaucher.com>
2025-06-26 11:44:11 -0600

    Added a Backend dataclass ✨

---
+++ backend.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+from urllib.parse import ParseResult, urlparse
+from threading import RLock
+import requests
+
+@dataclass
+class Backend:
+    url: ParseResult
+    alive: bool = True
+    _lock: RLock = field(default_factory=RLock)
+    _session: requests.Session = field(default_factory=requests.Session)
+
+    def __post_init__(self):
+        if isinstance(self.url, str):
+            self.url = urlparse(self.url)
